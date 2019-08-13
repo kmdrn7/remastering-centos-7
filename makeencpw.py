b76dcba00a242716f4be4d620db4373eabc20582
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2019-08-13 20:21:43 +0700

    rename to .sh

--- makeencpw.py
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if type python > /dev/null 2>&1; then
-    echo 'import crypt,getpass; print crypt.crypt(getpass.getpass(), "$6$JNhgtyGHty65%4rt")' | python -
-else
-    echo "Python tidak terinstall pada sistem anda;"
-fi
