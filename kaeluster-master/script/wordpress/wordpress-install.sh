a65d5a2c1c6b4344f3a4d833a4d9d7ded2109914
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2019-11-30 16:33:44 +0700

    finish wordpress provisioning using shell script

--- kaeluster-master/script/wordpress/wordpress-install.sh
+++ kaeluster-master/script/wordpress/wordpress-install.sh
@@ -1,36 +1,49 @@
 #!/bin/sh
-
-# Masuk menjadi sudo
-echo "#0 Preparing Environtment for Installing Wordpress ====="
 sudo -i
-yum -y install unzip
-cd /tmp/config
-unzip conf.zip
-rm conf.zip
-
 echo "========================================================="
 echo "========= INSTALLING WORDPRESS ON MASTER SERVER ========="
 echo "========================================================="
 echo ". . . ."
 echo " . . . ."
+
+echo "========================================================="
+echo "#0 Preparing Environtment for Installing Wordpress ======"
+echo "========================================================="
+cd /tmp/config
+echo "- installing unzip, yum-utils, vim"
+yum -y install unzip yum-utils vim
+echo "- unzipping file configuration"
+unzip config.zip
+echo "- disabling SELinux"
+mv SELinux /etc/selinux/config
+echo "- removing old configutation zip file"
+rm config.zip
+
+echo "========================================================="
 echo "#1 Installing NGINX Web Server =========================="
+echo "========================================================="
 sleep 2s
+echo "- adding public repository"
 yum -y install epel-release
-yum -y install nginx
+echo "- installing nginx"
+yum -y install httpd
 
+echo "========================================================="
 echo "#2 Enabling NGINX Web Server ============================"
+echo "========================================================="
 sleep 2s
-systemctl start nginx
-systemctl enable nginx
-systemctl status nginx
-
+systemctl start httpd
+systemctl enable httpd
+systemctl status httpd
+echo "- add firewall rule for HTTP"
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
 setenforce 0
 
+echo "========================================================="
 echo "#3 Installing MariaDB ==================================="
+echo "========================================================="
 sleep 2s
-
 cat <<EOT >> /etc/yum.repos.d/mariadb.repo
 [mariadb]
 name = MariaDB
@@ -38,43 +51,44 @@ baseurl = http://yum.mariadb.org/10.3/rhel7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOT
-
 yum -y install mariadb-server mariadb
 systemctl start mariadb
 systemctl enable mariadb
+echo "- change default mysql password to root"
 mysqladmin -u root password 'root'
 
+echo "========================================================="
 echo "#4 Installing PHP 7 ====================================="
+echo "========================================================="
 sleep 2s
+echo "- add php7 repository"
 rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum -y install yum-utils
-yum-config-manager --enable remi-php73
-yum -y install php php-mysql php-fpm php-gd php-xml php-mbstring php-opcache php-devel php-pear php-bcmath
+yum-config-manager --enable remi-php72
+echo "- installing php plugin for wordpress"
+yum -y install php php-mysql php-gd php-xml php-mbstring php-opcache php-devel php-pear php-bcmath
 
-echo "#5 Enabling PHP 7 ======================================="
-sleep 2s
-mv /tmp/config/www.conf /etc/php-fpm.d/www.conf
-systemctl start php-fpm
-systemctl enable php-fpm
-
-echo "#6 Setup Directory For Wordpress Instalation ============"
-cp /tmp/config/wp.conf /etc/nginx/conf.d/
-mkdir /usr/share/nginx/wp
-mkdir /usr/share/nginx/wp/logs
-setenforce 0
-systemctl restart nginx
-systemctl restart php-fpm
-
-echo "#7 Setup MySQL Database for Wordpress ============"
+echo "========================================================="
+echo "#7 Setup MySQL Database for Wordpress ==================="
+echo "========================================================="
+echo "- create user and database for wordpress"
 mysql -u root -proot < /tmp/config/setup.sql
 
-echo "#8 Downloading Latest Wordpress Instalation ============"
-cd /tmp
+echo "========================================================="
+echo "#8 Downloading Latest Wordpress Instalation ============="
+echo "========================================================="
+cd /tmp/config
+echo "- extracting wordpress instalation files"
 tar -zxvf wp.tar.gz
-mv wordpress/* /usr/share/nginx/wp
-cp /tmp/config/wp-config.php /usr/share/nginx/wp/wp-config.php
-chown -R nginx:nginx /usr/share/nginx/wp/
+echo "- copying wordpress to nginx public folder"
+mv wordpress/* /var/www/html
+echo "- configuring wordpress"
+mv /tmp/config/wp-config.php /var/www/html/wp-config.php
+echo "- change wordpress folder owner to nginx"
+chown -R apache:apache /var/www/html
 
-echo "============================================"
-echo "======== MASTER SERVER IS INSTALLED ========"
-echo "============================================"
+echo "========================================================="
+echo "=============== MASTER SERVER IS INSTALLED =============="
+echo "========================================================="
+echo ". . . ."
+echo ". . "
+echo "."
